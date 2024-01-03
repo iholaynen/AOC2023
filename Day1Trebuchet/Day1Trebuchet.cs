d2ab8b7bb61458e116ad30faf14a3a9b2e58c52e
Vova Sadov <vovasadov@microsoft.com>
2024-01-03 14:02:46 +0000

    Puzzle 1.2

--- Day1Trebuchet/Day1Trebuchet.cs
+++ Day1Trebuchet/Day1Trebuchet.cs
@@ -2,12 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AOC2023.Day1Trebuchet
 {
     internal class Day1Trebuchet : AbstractPuzzle
     {
+        public enum Day1Type { Puzzle1, Puzzle2 }
+        private string m_pattern;
+        private static readonly Dictionary<string, int> m_digits = new Dictionary<string, int>
+        {
+            { "1", 1 },
+            { "2", 2 },
+            { "3", 3 },
+            { "4", 4 },
+            { "5", 5 },
+            { "6", 6 },
+            { "7", 7 },
+            { "8", 8 },
+            { "9", 9 },
+            { "0", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+            { "zero", 0 }
+        };
+
+        public Day1Trebuchet(Day1Trebuchet.Day1Type day1Type) { 
+            switch (day1Type)
+            {
+                case Day1Type.Puzzle1:
+                    m_pattern = "[0-9]";
+                    break;
+                case Day1Type.Puzzle2:
+                    m_pattern = "([0-9]|one|two|three|four|five|six|seven|eight|nine|zero)";
+                    break;
+
+            }
+        }
         public override string Solve()
         {
             int total = 0;
@@ -18,8 +57,9 @@ namespace AOC2023.Day1Trebuchet
 
         private int calcValue(string line)
         {
-            int first = line.FirstOrDefault(Char.IsDigit) - '0';
-            int last = line.LastOrDefault(Char.IsDigit) - '0';
+            int first = m_digits[Regex.Match(line, m_pattern).Value];
+      
+            int last = m_digits[Regex.Match(line, m_pattern, RegexOptions.RightToLeft).Value]; 
             return first * 10 + last;
         }
 
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// This is a personal sandbox project for AOC2023
 using AOC2023.Day1Trebuchet;
 
-Day1Trebuchet puzzle = new Day1Trebuchet();
+Day1Trebuchet puzzle = new Day1Trebuchet(Day1Trebuchet.Day1Type.Puzzle2);
 
 Console.WriteLine(puzzle.Solve());
