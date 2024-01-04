5e5a6db07b2dc076beff2ea4cdef24d453ae6940
Vova Sadov <vovasadov@microsoft.com>
2024-01-04 14:35:12 +0000

    Puzzle 2.1

---
+++ Day2CubeConundrum/Day2CubeConundrum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC2023.Day2CubeConundrum
+{
+    internal class Day2CubeConundrum : AbstractPuzzle
+    {
+        //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        private static string _patternLine = "Game\\s(?<game>\\d+):\\s(?<tuples>.*)";
+        private static string _patternTuple = @"(\d+) (red|blue|green)";
+        public override string Solve()
+        {
+            int total = 0;
+            foreach (string line in Day2CubeConundrum.ReadLines(Day2CubeConundrum.GetStandardInput()))
+                total += this.evaluateGame(line);
+            return total.ToString();
+        }
+
+        private int evaluateGame(string line)
+        {
+            Match gameMatch = Regex.Match(line, _patternLine);
+            int gameNum = int.Parse(gameMatch.Groups["game"].Value);
+
+            string[] tuples = gameMatch.Groups["tuples"].Value.Split(';');
+            int maxRed = 0;
+            int maxGreen = 0;
+            int maxBlue = 0;
+            foreach (string tuple in tuples)
+            {
+                MatchCollection matches = Regex.Matches(tuple, _patternTuple);
+                foreach (Match match in matches)
+                {
+                    int count = int.Parse(match.Groups[1].Value);
+                    string color = match.Groups[2].Value;
+                    switch (color)
+                    {
+                        case "red":
+                            maxRed = Math.Max(maxRed, count);
+                            break;
+                        case "green":
+                            maxGreen = Math.Max(maxGreen, count);
+                            break;
+                        case "blue":
+                            maxBlue = Math.Max(maxBlue, count);
+                            break;
+                    }
+                }
+            }
+
+            return (12>=maxRed && 13>=maxGreen && 14>=maxBlue)?gameNum:0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// This is a personal sandbox project for AOC2023
 using AOC2023.Day1Trebuchet;
+using AOC2023.Day2CubeConundrum;
 
-Day1Trebuchet puzzle = new Day1Trebuchet(Day1Trebuchet.Day1Type.Puzzle2);
-
+//Day1Trebuchet puzzle = 7new Day1Trebuchet(Day1Trebuchet.Day1Type.Puzzle1);
+//Day1Trebuchet puzzle = new Day1Trebuchet(Day1Trebuchet.Day1Type.Puzzle2);
+Day2CubeConundrum puzzle = new Day2CubeConundrum();
 Console.WriteLine(puzzle.Solve());
