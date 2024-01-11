cce61aa04dfd78a61e27fd4a2a0b59cb5d85d5b0
Vova Sadov <vovasadov@microsoft.com>
2024-01-11 17:46:55 +0000

    Puzzles 3.1 & 3.2

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 using AOC2023;
 using AOC2023.Day1Trebuchet;
 using AOC2023.Day2CubeConundrum;
+using AOC2023.Day3GearRatios;
 
 //Day1Trebuchet puzzle = 7new Day1Trebuchet(AbstractPuzzle.PuzzleType.Puzzle1);
 //Day1Trebuchet puzzle = new Day1Trebuchet(AbstractPuzzle.PuzzleType.Puzzle2);
 //Day2CubeConundrum puzzle = new Day2CubeConundrum(AbstractPuzzle.PuzzleType.Puzzle1);
-Day2CubeConundrum puzzle = new Day2CubeConundrum(AbstractPuzzle.PuzzleType.Puzzle2);
+//Day2CubeConundrum puzzle = new Day2CubeConundrum(AbstractPuzzle.PuzzleType.Puzzle2);
+//Day3GearRatios puzzle = new Day3GearRatios(AbstractPuzzle.PuzzleType.Puzzle1);
+Day3GearRatios puzzle = new Day3GearRatios(AbstractPuzzle.PuzzleType.Puzzle2);
 Console.WriteLine(puzzle.Solve());
